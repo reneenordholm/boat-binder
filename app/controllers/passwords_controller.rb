c2607fd4ed6775984637ca43f17d9f2b17b20b6e
Renee Nordholm <juliereneehayes@gmail.com>
2026-06-21 09:41:37 -0700

    Handle password reset delivery failures

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,4 +1,13 @@
 class PasswordsController < ApplicationController
+  RESET_REQUEST_NOTICE = "If an account exists for that email, reset instructions will be sent shortly."
+  MAIL_DELIVERY_ERRORS = [
+    Net::SMTPError,
+    IOError,
+    SocketError,
+    SystemCallError,
+    Timeout::Error
+  ].freeze
+
   allow_unauthenticated_access
   before_action :set_user_by_token, only: %i[ edit update ]
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to new_password_path, alert: "Try again later." }
@@ -8,11 +17,10 @@ class PasswordsController < ApplicationController
 
   def create
     if user = User.find_by(email_address: params[:email_address])
-      PasswordsMailer.reset(user).deliver_now
-      Rails.logger.info("Password reset email delivered for user_id=#{user.id}")
+      deliver_password_reset(user)
     end
 
-    redirect_to new_session_path, notice: "Password reset instructions sent (if user with that email address exists)."
+    redirect_to new_session_path, notice: RESET_REQUEST_NOTICE
   end
 
   def edit
@@ -28,6 +36,15 @@ class PasswordsController < ApplicationController
   end
 
   private
+    def deliver_password_reset(user)
+      PasswordsMailer.reset(user).deliver_now
+      Rails.logger.info("Password reset email delivered for user_id=#{user.id}")
+    rescue *MAIL_DELIVERY_ERRORS => error
+      Rails.logger.error(
+        "Password reset email delivery failed for user_id=#{user.id}: #{error.class}: #{error.message}"
+      )
+    end
+
     def set_user_by_token
       @user = User.find_by_password_reset_token!(params[:token])
     rescue ActiveSupport::MessageVerifier::InvalidSignature
